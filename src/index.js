2b24a6068c02cde730f9792c5ee6ebcfae6733fd
chiccomotta <cristiano.motta@hotmail.it>
2020-02-05 10:47:03 +0100

    added label component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import Counter from './components/Counter'
-import counter from './reducers'
+import React from "react"
+import { render } from "react-dom"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+import Counter from "./components/Counter"
+import Label from "./components/Label"
+import counter from "./reducers"
+import "./site.css"
 
 const store = createStore(counter)
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById("root")
 
 render(
   <Provider store={store}>
     <Counter />
+    <div>
+      <Label />
+    </div>
   </Provider>,
   rootEl
 )
