fc929fe061d2ab40ce2950bb233fd85857fd918e
chiccomotta <cristiano.motta@hotmail.it>
2020-02-05 10:47:55 +0100

    added files

---
+++ src/components/Label.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+
+class Label extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { value } = this.props
+    return (
+      <div className="container">
+        <p>{value}</p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return { value: state }
+}
+
+export default connect(mapStateToProps, null)(Label)
